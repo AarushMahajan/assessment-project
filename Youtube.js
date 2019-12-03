14089c1ae060b2661fa9a3f8e119a35bc17a7d9d
Aarush Mahajan <50150154+AarushMahajan@users.noreply.github.com>
2019-12-03 10:31:59 +0530

    Update Youtube.js

--- Youtube.js
+++ Youtube.js
@@ -13,7 +13,7 @@ function fun(){
 
 
 
-var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${x}&key=AIzaSyDuuENadGsxMfvmUMKQDt1dmxlP_t6hYAY`
+var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${x}&key=AIzaSyBwOUqjVOuNxnmy91ilJ9UsL10gs7aFKBw`
 var xHR = new XMLHttpRequest();
 xHR.open('Get',url);
 
@@ -60,7 +60,7 @@ xHR.onreadystatechange = function(){
 
     // comment api
 
-    var url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=AIzaSyDuuENadGsxMfvmUMKQDt1dmxlP_t6hYAY&videoId=${comment_videoId}&maxResults=20`
+    var url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=AIzaSyBwOUqjVOuNxnmy91ilJ9UsL10gs7aFKBw&videoId=${comment_videoId}&maxResults=20`
     var xHR1 = new XMLHttpRequest();
     xHR1.open('Get',url);
     
